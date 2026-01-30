8690997966cd5a4b8e293b91e282713c25e984ff
Edsonfadez <edsonbarbeerr@gmail.com>
2026-01-30 02:31:52 -0300

    adiciona escolha de barbeiro

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let barbeiroSelecionado = null;
 let servicoSelecionado = null;
 let precoSelecionado = null;
 
@@ -5,12 +6,30 @@ function selecionarServico(servico, preco) {
   servicoSelecionado = servico;
   precoSelecionado = preco;
   alert(`Serviço selecionado: ${servico}`);
+}function selecionarBarbeiro(nome) {
+  barbeiroSelecionado = nome;
+  alert(`Barbeiro selecionado: ${nome}`);
 }
 
 function confirmarAgendamento() {
   const data = document.getElementById("data").value;
   const hora = document.getElementById("hora").value;
 
+  if (!servicoSelecionado || !barbeiroSelecionado || !data || !hora) {
+    alert("Preencha tudo antes de confirmar.");
+    return;
+  }
+
+  const resumo = `
+Serviço: ${servicoSelecionado}
+Barbeiro: ${barbeiroSelecionado}
+Valor: R$${precoSelecionado}
+Data: ${data}
+Horário: ${hora}
+  `;
+
+  document.getElementById("resumo").innerText = resumo;
+}
   if (!servicoSelecionado || !data || !hora) {
     alert("Preencha tudo antes de confirmar.");
     return;
